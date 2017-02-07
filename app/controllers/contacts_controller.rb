ddc8c7905ffe5894303dabea9a50cc07fdfa5c18
KennyBanna <gravadlaxo93@hotmail.com>
2017-02-07 20:21:10 +0000

    Added contact form fields

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,17 @@
 class ContactsController < ApplicationController
 
   def new
-    
+    @contact = Contact.new
   end 
+  
+  def create
+    #creates contact object with mass assignment
+    @contact = Contact( contact_params )
+  end
+  
+  #Whitelisting form fields as best practice security feature 
+  def contact_params
+    params.require(:contacts).permit(:name, :email, :contacts)
+  end
   
 end
