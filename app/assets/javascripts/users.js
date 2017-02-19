d91de8d1f993418e938c4985e397b5d0f85d26df
KennyBanna <gravadlaxo93@hotmail.com>
2017-02-19 17:45:14 +0000

    Added stripe response handler and error handling JS

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,13 +12,15 @@ $(document).on('turbolinks:load', function(){
   signupBtn.click(function(event){
     //prevent default event.
     event.preventDefault();
-    
+    //disable button.
+    signupBtn.val("Processing").prop('disabled', true);
+      
     //Collect card fields.
     var cardNumber =  $('#card_number').val(),
         cardCvc = $('#card_code').val(),
         cardExpMonth = $('#card_month').val(),
         cardExpYear = $('#card_year').val();
-    
+      
     //Send fields to stripe  
     Stripe.card.createToken({
       number: cardNumber,
@@ -26,12 +28,30 @@ $(document).on('turbolinks:load', function(){
       exp_month: cardExpMonth,
       exp_year: cardExpYear
     }, stripeResponseHandler);
-    
+  
   });
+    
+  function stripeResponseHandler(status, response){
+    
+    //If the card info contained no errors
+    if(!response.error){
+      
+      //Get token from response
+      var token = response.id
+      
+      //Inject field as hidden field in to form
+      theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+      
+      //Save customer to rails app
+      theForm.get(0).submit;
+      
+    } else {
+      //Display error message
+      alert(response.error.message);
+      //enable button
+      signupBtn.val("Sign Up").prop('disabled', true);
+    }
+    
+  }
   
-  
-
-  //Inject field as hidden field in to form
-  
-  //Save customer to rails app
 });
