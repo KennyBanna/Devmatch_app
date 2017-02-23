8e3b9a4c8c2b316bf7c7c57b1670f797f0576b6b
KennyBanna <gravadlaxo93@hotmail.com>
2017-02-23 21:36:54 +0000

    Added create action for User profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,31 @@
 class ProfilesController < ApplicationController
   
   def new
-    @profile = Profile.new
+    if user_signed_in?
+      @profile = Profile.new
+    else
+      redirect_to root_path
+    end
   end
   
   def create
     
+    @user = current_user
+    #build_association method comes from the "has_one" relationsship  
+    @user.build_profile( profile_parameters )
+    
+    if @user.save
+      flash[:success] = 'Successfully created profile'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+    
   end
+  
+  private 
+    def profile_parameters
+      params.require(:profile).permit(:fname, :lname, :title, :description) 
+    end
   
 end
