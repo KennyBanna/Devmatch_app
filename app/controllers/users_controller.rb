cf970df98a73b99f7e9df697453606709b784b06
KennyBanna <gravadlaxo93@hotmail.com>
2017-02-26 13:00:39 +0000

    Added additional style and functionality to users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user!
+  
   #GET request to users/:id
   def show
     @user = User.find(params[:id])
   end
   
+  #GET request to users
   def index
-    @users = User.all
+    #SQL join (all Users and include all profiles)
+    @users = User.includes(:profile)
   end
   
 end
