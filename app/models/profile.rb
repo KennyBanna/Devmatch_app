61ed24896a2d94edb7d6b815a4b67fdde958d03b
KennyBanna <gravadlaxo93@hotmail.com>
2017-02-28 11:55:35 +0000

    Added avatar to profile table and profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,10 @@ class Profile < ApplicationRecord
   
   belongs_to :user
   
+  # Profile avatar
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
   validates :fname, :lname, presence: true
   validates :description, length: {in: 5..100}
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221212110) do
+ActiveRecord::Schema.define(version: 20170228115046) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -32,9 +32,13 @@ ActiveRecord::Schema.define(version: 20170221212110) do
     t.string   "lname"
     t.string   "title"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
